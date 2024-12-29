f9c09318be278bdb6248b8e5c87a36c415fbae82
Nicolas Arellano Guzman <narel001@campus.goldmsmiths.ac.uk>
2024-12-29 14:53:32 +0100

    Added user dependant views & staff login

--- db.sql
+++ db.sql
@@ -7,7 +7,7 @@ USE RELAY;
 # Create the tables
 CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT,name VARCHAR(40),email CHAR(16), password VARCHAR(20), primary key (id));
 CREATE TABLE IF NOT EXISTS staff (id INT AUTO_INCREMENT,name VARCHAR(40),email CHAR(16), password VARCHAR(20), primary key (id));
-CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT, message VARCHAR(255), student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), teacher_id INT, FOREIGN KEY (teacher_id) REFERENCES staff(id), primary key(id));
+CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT, message VARCHAR(255), student_id INT, FOREIGN KEY (student_id) REFERENCES students(id), teacher_id INT, FOREIGN KEY (teacher_id) REFERENCES staff(id), primary key(id), timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
 
 
 CREATE USER IF NOT EXISTS 'relay_app'@'localhost' IDENTIFIED BY 'relay_password'; 
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const router = express.Router();
 
 var relayData = {pageName: "RELAY"};
+var userData ={};
 
 //Handle our routes
 router.get('/',function(req,res){
@@ -12,18 +13,13 @@ router.get('/about',function(req,res){
     res.render('about.ejs', relayData)
 });
 
-router.get('/communication',function(req,res){
-    if (req.session.loggedin) {
-        res.render('comm.ejs', relayData)
-    } else {
-        res.send('Please log for full access!')
-    }
-});
-
 router.get('/login',function(req,res){
-    res.render('login.ejs', relayData)
+    res.render('studentLogin.ejs', relayData)
 });
 
+router.get('/staff-login',function(req,res){
+    res.render('staffLogin.ejs', relayData)
+})
 
 //Load page with user profile
 router.post('/home',function(req,res){
@@ -35,23 +31,97 @@ router.post('/home',function(req,res){
     if (email && password) {
 
         //Query the database for student email and password
-        db.query('SELECT * FROM students WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+        const studentQuery = 'SELECT * FROM students WHERE email = ? AND password = ?';
+        db.query(studentQuery, [email, password], function(error, results, fields) {
 
             if (error) throw error;
             //If fields match the database -> log the user in and redirect back home page
             if(results.length > 0) {
-                req.session.loggedin = true;
+                req.session.studentLoggedin = true;
                 req.session.email = email;
 
-                let userData = Object.assign({}, relayData, {userInfo:results});
-                res.render('loggedin.ejs', userData);
+                //Create a new object for active student data to get sent to the app
+                userData = Object.assign({}, relayData, {userInfo:results});
+                res.render('studentLoggedin.ejs', userData);
             } else {
-                res.send('Incorrect username or password');
+                //In case of invalid email | password
+                res.status(401).json({ error: 'Incorrect username or password'})
             }
             res.end();
         });
     } else {
-        res.send('Please enter email and password');
+        //If fields are missing
+        res.send('Please enter login credentials');
+    }
+});
+
+//Load page with staff user profile
+router.post('/staff-home',function(req,res){
+    //Store inputted fields
+    let email = req.body.Email;
+    let password = req.body.Pword;
+
+    //Make sure fields aren't empty
+    if (email && password) {
+
+        //Query the database for student email and password
+        const staffQuery = 'SELECT * FROM staff WHERE email = ? AND password = ?';
+        db.query(staffQuery, [email, password], function(error, results, fields) {
+
+            if (error) throw error;
+            //If fields match the database -> log the user in and redirect back home page
+            if(results.length > 0) {
+                req.session.staffLoggedin = true;
+                req.session.email = email;
+
+                //Create a new object for active student data to get sent to the app
+                userData = Object.assign({}, relayData, {userInfo:results});
+                res.render('staffLoggedin.ejs', userData);
+            } else {
+                //In case of invalid email | password
+                res.status(401).json({ error: 'Incorrect username or password'})
+            }
+            res.end();
+        });
+    } else {
+        //If fields are missing
+        res.send('Please enter login credentials');
+    }
+});
+
+//Handle routes if logged in
+
+//for home button
+router.get('/student-home',function(req,res){
+    if (req.session.studentLoggedin) {
+        res.render('studentLoggedin.ejs', userData)
+    } else {
+        res.send('Please log in for full access!')
+    }
+});
+
+router.get('/staff-home',function(req,res){
+    if (req.session.staffLoggedin) {
+        res.render('staffLoggedin.ejs', userData)
+    } else {
+        res.send('Please log in for full access!')
+    }
+});
+
+//for personalized communication boards
+router.get('/student-communication',function(req,res){
+    if (req.session.studentLoggedin) {
+        res.render('studentComm.ejs', userData)
+    } else {
+        res.send('Please log in for full access!')
+    }
+});
+
+router.get('/staff-communication',function(req,res){
+    if (req.session.staffLoggedin) {
+        res.render('staffComm.ejs', userData)
+    } else {
+        res.send('Please log in for full access!')
     }
 });
 
