3bc3000d3dafbbe4660337b2e0801ffc59fb9dd7
Nicolas Arellano Guzman <narel001@campus.goldmsmiths.ac.uk>
2025-01-04 15:03:13 +0100

    small fix

--- db.sql
+++ db.sql
@@ -5,8 +5,6 @@ CREATE DATABASE IF NOT EXISTS RELAY;
 USE RELAY;
 
 # Create the tables
---CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT,name VARCHAR(40),email CHAR(16), password VARCHAR(20), primary key (id));
---CREATE TABLE IF NOT EXISTS staff (id INT AUTO_INCREMENT,name VARCHAR(40),email CHAR(16), password VARCHAR(20), primary key (id));
 CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(40), email CHAR(16), password VARCHAR(20), role ENUM('student', 'staff'))
 CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT, message VARCHAR(255), sender_id INT, FOREIGN KEY (sender_id) REFERENCES users(id), recipient_id INT, FOREIGN KEY (recipient_id) REFERENCES users(id), primary key(id), timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
 
