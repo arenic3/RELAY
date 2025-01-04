0dc551bd6b7f2dc5f259ac81c081b8291d709fe8
Nicolas Arellano Guzman <narel001@campus.goldmsmiths.ac.uk>
2025-01-04 15:10:39 +0100

    small fix

--- db.sql
+++ db.sql
@@ -5,8 +5,10 @@ CREATE DATABASE IF NOT EXISTS RELAY;
 USE RELAY;
 
 # Create the tables
+--CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT,name VARCHAR(40),email CHAR(16), password VARCHAR(20), primary key (id));
+--CREATE TABLE IF NOT EXISTS staff (id INT AUTO_INCREMENT,name VARCHAR(40),email CHAR(16), password VARCHAR(20), primary key (id));
 CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, name VARCHAR(40), email CHAR(16), password VARCHAR(20), role ENUM('student', 'staff'), primary key(id))
-CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT, message VARCHAR(255), sender_id INT, FOREIGN KEY (sender_id) REFERENCES users(id), recipient_id INT, FOREIGN KEY (recipient_id) REFERENCES users(id), primary key(id), timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT, message VARCHAR(255), sender_id INT,  recipient_id INT, primary key(id), timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (sender_id) REFERENCES users(id), FOREIGN KEY (recipient_id) REFERENCES users(id));
 
 CREATE USER IF NOT EXISTS 'relay_app'@'localhost' IDENTIFIED BY 'relay_password'; 
 GRANT ALL PRIVILEGES ON RELAY.* TO 'relay_app'@'localhost';
